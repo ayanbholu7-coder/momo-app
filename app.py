020991308afa8eb44e4ca86d8a1a848b5fbec26b
ayanbholu7-coder <ayanbholu7@gmail.com>
2026-07-26 15:41:31 +0500

    app.py

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ with tab1:
         else:
             try:
                 genai.configure(api_key=gemini_key)
-                model = genai.GenerativeModel("gemini-pro")
+                model = genai.GenerativeModel("gemini-1.5-flash-latest")
 
                 prompt = f"""
                 Extract customer details from this message: "{raw_text}".
